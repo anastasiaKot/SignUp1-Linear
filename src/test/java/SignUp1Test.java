c83fbaea41bb25d069cac2dda83fb34f21c6f1a8
anastasiashpak <anastasia.shpak@ymedialabs.com>
2016-06-11 14:49:29 -0700

    unable to connect.

--- src/test/java/SignUp1Test.java
+++ src/test/java/SignUp1Test.java
@@ -1,47 +1,57 @@
-import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import java.util.concurrent.TimeUnit;
-
-import java.util.concurrent.TimeUnit;
-
-
-public class SignUp1Test
-{
-    public static  SignUp1 appjava = new SignUp1();
-    static String aidee = null;
-
-    @Test
-    public void field_FirstNameIsPresent() {
-        aidee = "id_f_title";
-        boolean exist = SignUp1.presentOrNot(aidee);
-        Assert.assertTrue(exist, "MEME");
-        SignUp1.close();
-
-    }
-
-    @Test
-    public void field_LastNameIsPresent()
-    {
-        boolean exist = SignUp1.presentOrNot("id_l_tditssle");
-
-        Assert.assertTrue(exist, "BEBE");
-        SignUp1.close();
-
-    }
-
-
-
-
-
-
-}
+//import org.junit.Before;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.NoSuchElementException;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.Assert;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.NoSuchElementException;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import java.util.concurrent.TimeUnit;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//
+//public class SignUp1Test
+//{
+//    //static WebDriver driver = new FirefoxDriver();
+//    static  SignUp1 appjava = new SignUp1();
+//    static String aidee = null;
+//
+////    @BeforeClass
+////    public static  void setUpBeforeClass() throws Exception
+////    {
+////        driver.get("http://learn2test.net/qa/apps/sign_up/v1/");
+////    }
+////
+////    @AfterClass
+////    public static void tearDownAfterClass() throws Exception
+////    {
+////        driver.close();
+////        driver.quit();
+////    }
+//
+//    @Test
+//    public void field_FirstNameIsPresent()
+//    {
+//        aidee = "id_f_title";
+//        boolean exist = SignUp1.presentOrNot(aidee);
+//        Assert.assertTrue(exist, "MEME");
+//        SignUp1.close();
+//    }
+//
+//    @Test
+//    public void field_LastNameIsPresent()
+//    {
+//        boolean exist = SignUp1.presentOrNot("id_l_tditssle");
+//
+//        Assert.assertTrue(exist, "BEBE");
+//        SignUp1.close();
+//
+//    }
+//}
